a9bb6dfcf8afb665654afbe03696f794e6a5a685
qkwmapel <otxoid@gmail.com>
2024-04-03 10:09:16 +0900

    [Feat]#2-Add TitleView

--- OrderSo10Jo/Controller/ViewController.swift
+++ OrderSo10Jo/Controller/ViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var TitleView: UIView!
   override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view.
-    TitleView.backgroundColor = .green
    }
 
 
