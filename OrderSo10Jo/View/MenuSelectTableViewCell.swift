4e69f7bb9bb854ca9a937649b6dd6d9e3696cc53
suri0000 <yesle2005@naver.com>
2024-04-05 00:33:15 +0900

    [Feat]#11-메뉴 누르면 주문내역에 보여주기

--- OrderSo10Jo/View/MenuSelectTableViewCell.swift
+++ OrderSo10Jo/View/MenuSelectTableViewCell.swift
@@ -12,11 +12,11 @@ class MenuSelectTableViewCell: UITableViewCell {
     //타입 프로퍼티 변수 추가로 테이블뷰 셀의 identifier을 지정
     static let identifier = "MenuSelectTableViewCell"
     
-    @IBOutlet weak var DrinkImage: UIImageView!
+    @IBOutlet weak var drinkImage: UIImageView!
     
-    @IBOutlet weak var DrinkName: UILabel!
+    @IBOutlet weak var drinkName: UILabel!
     
-    @IBOutlet weak var DrinkCost: UILabel!
+    @IBOutlet weak var drinkCost: UILabel!
     
     
     
